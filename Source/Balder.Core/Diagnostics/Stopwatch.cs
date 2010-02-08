fcc309c8760ac42fbdc00f0103272901ccb79577
Einar Ingebrigtsen <einar@dolittle.com>
2010-02-08 19:55:46 +0100

    Giving credit where credit is due.

--- Source/Balder.Core/Diagnostics/Stopwatch.cs
+++ Source/Balder.Core/Diagnostics/Stopwatch.cs
@@ -1,27 +1,8 @@
-#region License
-
-//
-// Author: Einar Ingebrigtsen <einar@dolittle.com>
-// Copyright (c) 2007-2010, DoLittle Studios
-//
-// Licensed under the Microsoft Permissive License (Ms-PL), Version 1.1 (the "License")
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the license at 
-//
-//   http://balder.codeplex.com/license
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
-#endregion
 using System;
 
 namespace Balder.Core.Diagnostics
 {
+	// Based upon Tiaan Gelenhuys Stopwatch : http://blog.tiaan.com/link/2009/02/03/stopwatch-silverlight
 	public class Stopwatch
 	{
 		public static readonly bool IsHighResolution = false;
